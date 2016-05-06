a4da7186bab8f23727bbbabacb9b4a67e3f0f2e1
v1k45 <v1k45@users.noreply.github.com>
2016-05-06 21:51:42 +0530

    Added UserProfile models; made migrations

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    avatar = models.ImageField(blank=True)
+    about = models.TextField(blank=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+    def save(self, *args, **kwargs):
+        if self.id and self.avatar:
+            current_avatar = UserProfile.objects.get(pk=self.id).avatar
+            if current_avatar != self.avatar:
+                current_avatar.delete()
+        super(UserProfile, self).save(*args, **kwargs)
