8aec4e7a574e75895a2961e8062ef6e07fafee58
v1k45 <v1k45@users.noreply.github.com>
2016-05-07 00:38:21 +0530

    User profile view and template

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def user_profile(request, username):
+    # getting default user model
+    UserModel = get_user_model()
+    profile = get_object_or_404(
+        UserModel.objects.select_related(), username=username
+    ).profile
+    return render(request, 'authapp/user_profile.html', {'profile': profile})
