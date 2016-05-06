ea79066d7f8848d30f21c09fd8bc85f4b200db92
v1k45 <v1k45@users.noreply.github.com>
2016-05-07 00:40:46 +0530

    Tests for user profile view

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from .models import UserProfile
+
+
+class UserProfileViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test_user', email='test_user@email.com',
+            password='test_password')
+        profile = UserProfile(user=self.user)
+        profile.save()
+
+    def test_view_profile_is_accessible(self):
+        target = reverse('profile', args=[self.user.username])
+        response = self.client.get(target)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authapp/user_profile.html')
+        self.assertEqual(response.context['profile'], self.user.profile)
