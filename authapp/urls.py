cc9f6a571c2a6139c6b6ba5e4dda45f864eea9f9
v1k45 <v1k45@users.noreply.github.com>
2016-05-08 01:05:06 +0530

    Profile redirector view and url conf

--- authapp/urls.py
+++ authapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^u/(?P<username>[\w.@+-]+)/$', views.user_profile, name='profile'),
+    url(r'^@(?P<username>[\w-]+)/$', views.user_profile, name='profile'),
+    url(r'^accounts/profile/', views.profile_redirector, name='user_profile'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 
 def user_profile(request, username):
@@ -9,3 +10,8 @@ def user_profile(request, username):
         UserModel.objects.select_related(), username=username
     ).profile
     return render(request, 'authapp/user_profile.html', {'profile': profile})
+
+
+@login_required
+def profile_redirector(request):
+    return redirect('profile', username=request.user.username)
