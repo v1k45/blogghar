417a885ef8030ae87aef029f86e5b6d1c1cd4824
v1k45 <v1k45@users.noreply.github.com>
2016-05-09 02:58:56 +0530

    Added url entry for blog create and update view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^@(?P<username>[\w-]+)/blog/$', views.blog_detail, name='user_blog'),
+    url(r'^blog-home/start/', views.blog_create, name='blog_create'),
+    url(r'^blog-home/update/', views.blog_update, name='blog_update'),
 ]
