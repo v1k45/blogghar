b7fafb41244f6ef2e819b3997a5ea939ce1374d3
v1k45 <v1k45@users.noreply.github.com>
2016-05-11 04:12:06 +0530

    Added Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from blog.models import Post
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               related_name='comments')
+    comment = models.TextField(max_length=500)
+    post = models.ForeignKey(Post, related_name='comments')
+
+    is_removed = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
