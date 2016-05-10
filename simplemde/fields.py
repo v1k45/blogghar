bfad975bfaa6318f2a673f5ac18590a9aee7c537
v1k45 <v1k45@users.noreply.github.com>
2016-05-10 15:05:28 +0530

    support for SimpleMDE model/form fields

---
+++ simplemde/fields.py
@@ -0,0 +1,17 @@
+from django.db.models import TextField
+
+from .widgets import SimpleMdeWidget
+
+
+class SimpleMdeField(TextField):
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleMdeField, self).__init__(*args, **kwargs)
+        # using SimpleMdeWidget for TextField
+        self.widget = SimpleMdeWidget()
+
+    def formfield(self, **kwargs):
+        defaults = {'widget': self.widget}
+        defaults.update(kwargs)
+        defaults['label'] = ''
+        return super(SimpleMdeField, self).formfield(**defaults)
