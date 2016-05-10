e80014efea2f20ad32c6826ea1ce5b26f2afe427
v1k45 <v1k45@users.noreply.github.com>
2016-05-10 18:30:55 +0530

    Added view to upload image and add entry to database

--- simplemde/views.py
+++ simplemde/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.http.response import JsonResponse
+from django.views.decorators.http import require_POST
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from blog.decorators import blogger_required
+from .models import ImageUpload
+
+
+class ImageUploadView(CreateView):
+    model = ImageUpload
+    fields = ['image']
+
+    @method_decorator([login_required, require_POST, blogger_required])
+    def dispatch(self, request, *args, **kwargs):
+        return super(ImageUploadView, self).dispatch(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        return JsonResponse({'success': False})
+
+    def form_valid(self, form):
+        form_data = form.save(commit=False)
+        form_data.uploader = self.request.user
+        form_data.save()
+        return JsonResponse({'success': True, 'imgURL': form_data.image.url})
