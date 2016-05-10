fd4b03d8ccf625369b5064916e369b05321fbe4f
v1k45 <v1k45@users.noreply.github.com>
2016-05-10 18:25:38 +0530

    Added model to store uploaded images by bloggers

--- simplemde/models.py
+++ simplemde/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class ImageUpload(models.Model):
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 related_name='image_uploads')
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image.name
