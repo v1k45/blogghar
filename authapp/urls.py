ca39609d2e5d856dd0c5b1b4d2f8d887d406ed7a
v1k45 <v1k45@users.noreply.github.com>
2016-05-11 15:39:54 +0530

    added user comments view

--- authapp/urls.py
+++ authapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^@(?P<username>[\w-]+)/$', views.UserProfile.as_view(), name='profile'),
+    url(r'^@(?P<username>[\w-]+)/$', views.UserProfileView.as_view(), name='profile'),
+    url(r'^@(?P<username>[\w-]+)/comments/$', views.UserComments.as_view(), name='comments'),
     url(r'^accounts/profile/', views.profile_redirector, name='user_profile'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,12 +1,14 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+from django.views.generic.detail import SingleObjectMixin
+from django.db.models import Prefetch
 
 from .models import UserProfile
+from comments.models import Comment
 
 
-class UserProfile(DetailView):
+class UserProfileView(DetailView):
     template_name = 'authapp/user_profile.html'
     model = UserProfile
     context_object_name = 'profile'
@@ -22,3 +24,28 @@ class UserProfile(DetailView):
 @login_required
 def profile_redirector(request):
     return redirect('profile', username=request.user.username)
+
+
+class UserComments(SingleObjectMixin, ListView):
+    template_name = 'authapp/user_profile.html'
+    model = UserProfile
+    slug_url_kwarg = 'username'
+    slug_field = 'user__username'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(
+            queryset=self.model.objects.select_related(
+                'user', 'user__blog').prefetch_related(Prefetch(
+                    'user__comments',
+                    queryset=Comment.objects.select_related('post').filter(is_public=True)  # noqa
+                    ))
+            )
+        return super(UserComments, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserComments, self).get_context_data(**kwargs)
+        context['profile'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.user.comments.all()
