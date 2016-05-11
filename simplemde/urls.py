6f0f3790ea33bfea19c6f37966bd8a00959d6a84
v1k45 <v1k45@users.noreply.github.com>
2016-05-11 21:00:01 +0530

    view to convert markdown to text

--- simplemde/urls.py
+++ simplemde/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^upload/$', views.ImageUploadView.as_view(), name='upload'),
+    url(r'^md2html/$', views.MarkdownToHTML.as_view(), name='md2html'),
 ]
--- simplemde/views.py
+++ simplemde/views.py
@@ -1,11 +1,12 @@
-from django.views.generic import CreateView
-from django.http.response import JsonResponse
+from django.views.generic import CreateView, View
+from django.http.response import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 from blog.decorators import blogger_required
 from .models import ImageUpload
+from .utils import md2html
 
 
 class ImageUploadView(CreateView):
@@ -24,3 +25,18 @@ class ImageUploadView(CreateView):
         form_data.uploader = self.request.user
         form_data.save()
         return JsonResponse({'success': True, 'imgURL': form_data.image.url})
+
+
+class MarkdownToHTML(View):
+
+    @method_decorator([login_required, require_POST, blogger_required])
+    def dispatch(self, request, *args, **kwargs):
+        return super(MarkdownToHTML, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        content = request.POST.get('content')
+        if content:
+            response = md2html(content)
+        else:
+            response = b""
+        return HttpResponse(response)
