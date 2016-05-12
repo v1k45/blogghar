fc3d91f23a0c5625f7305f9b1c57a3adce7bff3b
v1k45 <v1k45@users.noreply.github.com>
2016-05-12 14:21:41 +0530

    Filtered only public blog posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.assignment_tag
 def latest_post(user, limit=5):
-    qs = user.posts.published()[:limit]
+    qs = user.posts.published().filter(blog__is_public=True)[:limit]
     return qs
