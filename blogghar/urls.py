73b49ffec1a8ae4b5fdcd9637112b4f78c6ad8b6
v1k45 <v1k45@users.noreply.github.com>
2016-05-12 15:59:06 +0530

    Added media url confs for dev

--- blogghar/urls.py
+++ blogghar/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     url(r'^', include('authapp.urls', namespace='authapp')),
     url(r'^', include('blog.urls', namespace='blog')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # noqa
